8c5fa2a5e03d350193484eb3a23b6d469c8096e9
Ibrahim-AbuFarha <ibrahim.j.abufarha@gmail.com>
2023-07-28 22:06:41 +0300

    update slider

--- src/Components/Home/Slider.js
+++ src/Components/Home/Slider.js
@@ -1,146 +1,68 @@
 import React from "react";
-
-
+import { useState, useEffect } from "react";
+import "./Slider.css";
 function Slider() {
+  {
+    const [counter, setCounter] = useState(1);
 
-  return (
-    <div
-      id="default-carousel"
-      className="relative w-full"
-      data-carousel="static"
-    >
-      {/* Carousel wrapper */}
-      <div className="relative h-56 md:h-96 overflow-hidden rounded-lg">
-        {/* Item 1 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="https://images.pexels.com/photos/7857560/pexels-photo-7857560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        {/* Item 2 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="https://images.pexels.com/photos/7857560/pexels-photo-7857560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        {/* Item 3 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="https://images.pexels.com/photos/7857560/pexels-photo-7857560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        {/* Item 4 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="https://images.pexels.com/photos/7857560/pexels-photo-7857560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
-        </div>
-        {/* Item 5 */}
-        <div className="hidden duration-700 ease-in-out" data-carousel-item>
-          <img
-            src="https://images.pexels.com/photos/7857560/pexels-photo-7857560.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=1"
-            className="absolute block w-full -translate-x-1/2 -translate-y-1/2 top-1/2 left-1/2"
-            alt="..."
-          />
+    useEffect(() => {
+      const interval = setInterval(() => {
+        document.getElementById("radio" + counter).checked = true;
+        setCounter((prevCounter) => (prevCounter % 4) + 1);
+      }, 2000);
+
+      return () => clearInterval(interval);
+    }, [counter]);
+    return (
+      <div className="body">
+        <div className="slider">
+          <div className="slides">
+            <input type="radio" name="radio-btn" id="radio1" />
+            <input type="radio" name="radio-btn" id="radio2" />
+            <input type="radio" name="radio-btn" id="radio3" />
+            <input type="radio" name="radio-btn" id="radio4" />
+
+            <div className="slide first">
+              <img
+                src="https://media.discordapp.net/attachments/1125737184180326440/1134475050427351070/free-photo-of-close-up-of-a-playstation-joystick.png?width=670&height=670"
+                alt=""
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://media.discordapp.net/attachments/1125737184180326440/1134478387667271722/8ca4cdd64357ecddbf1f4c9109800ac3.png?width=670&height=670"
+                alt=""
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://media.discordapp.net/attachments/1125737184180326440/1134478458689441884/4eacc9a16fd0961ae8f70d5a26c29363.png?width=681&height=670"
+                alt=""
+              />
+            </div>
+            <div className="slide">
+              <img
+                src="https://media.discordapp.net/attachments/1125737184180326440/1134474950900732007/free-photo-of-black-wireless-headphones.png?width=670&height=670"
+                alt=""
+              />
+            </div>
+            <div className="navigation-auto">
+              <div className="auto-btn1"></div>
+              <div className="auto-btn2"></div>
+              <div className="auto-btn3"></div>
+              <div className="auto-btn4"></div>
+            </div>
+          </div>
+          <div className="navigation-manual ">
+            <label htmlFor="radio1" className="manual-btn"></label>
+            <label htmlFor="radio2" className="manual-btn"></label>
+            <label htmlFor="radio3" className="manual-btn"></label>
+            <label htmlFor="radio4" className="manual-btn"></label>
+          </div>
         </div>
       </div>
-      {/* Slider indicators */}
-      <div className="absolute z-30 flex space-x-3 -translate-x-1/2 bottom-5 left-1/2">
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="true"
-          aria-label="Slide 1"
-          data-carousel-slide-to="0"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 2"
-          data-carousel-slide-to="1"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 3"
-          data-carousel-slide-to="2"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 4"
-          data-carousel-slide-to="3"
-        ></button>
-        <button
-          type="button"
-          className="w-3 h-3 rounded-full"
-          aria-current="false"
-          aria-label="Slide 5"
-          data-carousel-slide-to="4"
-        ></button>
-      </div>
-      {/* Slider controls */}
-      <button
-        type="button"
-        className="absolute top-0 left-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-prev
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="M5 1 1 5l4 4"
-            />
-          </svg>
-          <span className="sr-only">Previous</span>
-        </span>
-      </button>
-      <button
-        type="button"
-        className="absolute top-0 right-0 z-30 flex items-center justify-center h-full px-4 cursor-pointer group focus:outline-none"
-        data-carousel-next
-      >
-        <span className="inline-flex items-center justify-center w-10 h-10 rounded-full bg-white/30 dark:bg-gray-800/30 group-hover:bg-white/50 dark:group-hover:bg-gray-800/60 group-focus:ring-4 group-focus:ring-white dark:group-focus:ring-gray-800/70 group-focus:outline-none">
-          <svg
-            className="w-4 h-4 text-white dark:text-gray-800"
-            aria-hidden="true"
-            xmlns="http://www.w3.org/2000/svg"
-            fill="none"
-            viewBox="0 0 6 10"
-          >
-            <path
-              stroke="currentColor"
-              stroke-linecap="round"
-              stroke-linejoin="round"
-              stroke-width="2"
-              d="m1 9 4-4-4-4"
-            />
-          </svg>
-          <span className="sr-only">Next</span>
-        </span>
-      </button>
-    </div>
-  );
+    );
+  }
 }
 
 export default Slider;
