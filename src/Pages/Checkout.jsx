438a35f481d00bb33def7cc4d2fe0d70d3a9e590
MohammadAwad97 <mohamad.s.awad97@gmail.com>
2023-07-29 17:56:06 +0300

    update cart

--- src/Pages/Checkout.jsx
+++ src/Pages/Checkout.jsx
@@ -263,45 +263,45 @@ const Checkout = () => {
               </Link>
             </div>
           </div>
-          <div className="flex flex-col justify-start items-start bg-gray-50 w-full p-6 md:p-14">
+          <div className="flex flex-col justify-start items-start bg-[#272525] w-full p-6 md:p-14">
             <div>
-              <h1 className="text-2xl font-semibold leading-6 text-gray-800">
+              <h1 className="text-2xl font-semibold leading-6 text-white">
                 Order Summary
               </h1>
             </div>
             <div className="flex mt-7 flex-col items-end w-full space-y-6">
               <div className="flex justify-between w-full items-center">
-                <p className="text-lg leading-4 text-gray-600">Total items</p>
-                <p className="text-lg font-semibold leading-4 text-gray-600">
+                <p className="text-lg leading-4 text-white">Total items :</p>
+                <p className="text-lg font-semibold leading-4 text-[#f2ee1b]">
                   20
                 </p>
               </div>
               <div className="flex justify-between w-full items-center">
-                <p className="text-lg leading-4 text-gray-600">Total Charges</p>
-                <p className="text-lg font-semibold leading-4 text-gray-600">
+                <p className="text-lg leading-4 text-white">Total Charges :</p>
+                <p className="text-lg font-semibold leading-4 text-[#f2ee1b]">
                   $2790
                 </p>
               </div>
               <div className="flex justify-between w-full items-center">
-                <p className="text-lg leading-4 text-gray-600">
-                  Shipping charges
+                <p className="text-lg leading-4 text-white">
+                  Shipping charges:
                 </p>
-                <p className="text-lg font-semibold leading-4 text-gray-600">
+                <p className="text-lg font-semibold leading-4 text-[#f2ee1b]">
                   $90
                 </p>
               </div>
               <div className="flex justify-between w-full items-center">
-                <p className="text-lg leading-4 text-gray-600">Sub total </p>
-                <p className="text-lg font-semibold leading-4 text-gray-600">
+                <p className="text-lg leading-4 text-white">Sub total :</p>
+                <p className="text-lg font-semibold leading-4 text-[#f2ee1b]">
                   $3520
                 </p>
               </div>
             </div>
             <div className="flex justify-between w-full items-center mt-32">
-              <p className="text-xl font-semibold leading-4 text-gray-800">
-                Estimated Total{' '}
+              <p className="text-xl font-semibold leading-4 text-white">
+                Estimated Total :{' '}
               </p>
-              <p className="text-lg font-semibold leading-4 text-gray-800">
+              <p className="text-lg font-semibold leading-4 text-[#f2ee1b]">
                 $2900
               </p>
             </div>
