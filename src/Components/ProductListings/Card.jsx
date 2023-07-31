82a34e7a2801c309ea0daa7d170d77c663c8aaac
abdelrahman-haroun <127291105+abdelrahman-haroun@users.noreply.github.com>
2023-07-31 11:51:44 +0300

    product cards

--- src/Components/ProductListings/Card.jsx
+++ src/Components/ProductListings/Card.jsx
@@ -13,27 +13,33 @@ function Card({ product }) {
     }
   };
   return (
-    <div key={product._id} className="group shadow-lg">
-      <div className=" flex justify-center aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
-        <img
-          src={product.img}
-          alt="img"
-          className="h-60 w-full object-cover object-center group-hover:opacity-75 "
-        />
+    <div
+      key={product._id}
+      className="max-w-sm bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700 overflow-hidden h-100 p-[1em]"
+    >
+      <img
+        className="h-56 object- fill w-full"
+        src={product.img}
+        alt={product.id}
+      />
+
+      <div className="p-2 h-70">
+        <p>
+          <h5 className="mb-2  font-bold tracking-tight text-gray-900 dark:text-white line-clamp-2 text-sm h-[40px]">
+            {product.name}
+          </h5>
+        </p>
+        <p className="mb-3 font-normal text-gray-700 dark:text-gray-200 line-clamp-3 text-xs h-[100px]">
+          {' '}
+          {product.desc}
+        </p>
+        <p
+          onClick={handelClick}
+          className="inline-flex items-center justify-center w-full px-3 py-2 font-medium text-center text-white  rounded-lg  hover:text-black focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600  dark:focus:ring-blue-800 text-xs bg-[#272525] hover:bg-[#b5b188]"
+        >
+          Add
+        </p>
       </div>
-      <h3 className="mt-4 text-m px-2 h-[30px] pb-[40px]">{product.name}</h3>
-      <p className="mt-4  px-2  text-gray-500 text-sm h-[190px]">
-        {product.desc}
-      </p>
-      <p className="px-2 mt-2 text-lg font-medium text-gray-900 ">
-        {product.price}$
-      </p>
-      <button
-        onClick={handelClick}
-        className="bg-[#272525]  hover:bg-[#b5b188]   hover:text-black   text-white font-bold py-2 px-4 rounded w-full mt-4"
-      >
-        Add
-      </button>
     </div>
   );
 }
